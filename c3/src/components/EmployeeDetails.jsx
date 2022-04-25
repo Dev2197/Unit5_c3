473ec95615d88d8a14d20da54c8b747a9a476230
Devendra <devendrakumart8@gmail.com>
2022-04-25 16:56:27 +0530

    commit

--- c3/src/components/EmployeeDetails.jsx
+++ c3/src/components/EmployeeDetails.jsx
@@ -4,6 +4,7 @@ import axios  from "axios";
 
 export const EmployeeDetails = () => {
     const [employ,setEmploy] = useState({});
+    const [task,setTask] = useState([]);
     const {id} = useParams();
 
     useEffect(()=>{
@@ -12,6 +13,8 @@ export const EmployeeDetails = () => {
          // handle success
          setEmploy(response.data)
         console.log(response.data);
+        console.log(response.data.tasks)
+        setTask(response.data.tasks)
   })
     },[]);
 
@@ -22,7 +25,7 @@ export const EmployeeDetails = () => {
         <h4 className="user_name">{employ.username}</h4>
         <span className="user_salary">${employ.salary}</span>
         <span className="tasks">
-        <li className="task"></li>
+        <li className="task">{task[0]}</li>
         </span>
         Status: <b className="status">{employ.status}</b>
         Title: <b className="title">{employ.title}</b>
